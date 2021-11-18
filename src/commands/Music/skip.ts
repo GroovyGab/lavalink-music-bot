f2eaadf4a6e597ff12b863e7b29f4ab56d91371c
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-18 18:13:37 -0300

    Add skip command

--- src/commands/Music/skip.ts
+++ src/commands/Music/skip.ts
@@ -2,8 +2,8 @@
  * Module imports.
  */
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command, CommandOptions } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import { Args, Command, CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
 	name: 'skip',
@@ -11,5 +11,72 @@ import type { Message } from 'discord.js';
 	fullCategory: ['music']
 })
 export class UserCommand extends Command {
-	public async messageRun(_message: Message) {}
+	public async messageRun(message: Message, args: Args) {
+		if (!message.guild) return;
+		const erelaPLayer = this.container.client.players.get(message.guild.id);
+		const embedReply = new MessageEmbed();
+		const { channel: userVoiceChannel } = message.member?.voice!;
+		const { channel: botVoiceChannel } = message.guild.me?.voice!;
+
+		try {
+			const skipAmmount = await args.rest('integer');
+
+			if (!userVoiceChannel) {
+				embedReply.setDescription('You have to be connected to a voice channel before you can use this command!').setColor('RED');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer) {
+				embedReply.setDescription("There isn't an active player on this server!").setColor('RED');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer.playing) {
+				embedReply.setDescription("There's nothing currently playing on this server!").setColor('RED');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (userVoiceChannel.id !== botVoiceChannel?.id) {
+				embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!').setColor('RED');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (skipAmmount > erelaPLayer.queue.length + 1) {
+				embedReply.setDescription("The ammount of songs to be skipped can't be larger than que queue's length!").setColor('RED');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			erelaPLayer.stop(skipAmmount);
+			return message.react('👌');
+		} catch (error: any) {
+			if (error.identifier === 'argsMissing') {
+				if (!userVoiceChannel) {
+					embedReply.setDescription('You have to be connected to a voice channel before you can use this command!').setColor('RED');
+					return message.reply({ embeds: [embedReply] });
+				}
+
+				if (!erelaPLayer) {
+					embedReply.setDescription("There isn't an active player on this server!").setColor('RED');
+					return message.reply({ embeds: [embedReply] });
+				}
+
+				if (!erelaPLayer.playing) {
+					embedReply.setDescription("There's nothing currently playing on this server!").setColor('RED');
+					return message.reply({ embeds: [embedReply] });
+				}
+
+				if (userVoiceChannel.id !== botVoiceChannel?.id) {
+					embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!').setColor('RED');
+					return message.reply({ embeds: [embedReply] });
+				}
+
+				erelaPLayer.stop();
+				return message.react('👌');
+			}
+
+			this.container.client.logger.error(`There was an unexpected error in command "${this.name}"`, error);
+			embedReply.setDescription('There was an unexpected error while processing the command, try again later.');
+			return message.reply({ embeds: [embedReply] });
+		}
+	}
 }
