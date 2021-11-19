65d5c3bf683b67e9710f056c2ed2117dc8e0420b
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-19 15:14:18 -0300

    feat(seek): added seek command

--- src/commands/Music/seek.ts
+++ src/commands/Music/seek.ts
@@ -11,11 +11,81 @@ import { Message, MessageEmbed } from 'discord.js';
 	fullCategory: ['music']
 })
 export class UserCommand extends Command {
-	public async messageRun(message: Message, _args: Args) {
+	public async messageRun(message: Message, args: Args) {
+		if (!message.guild) return;
+		const erelaPLayer = this.container.client.players.get(message.guild.id);
 		const embedReply = new MessageEmbed();
+		const { channel: userVoiceChannel } = message.member?.voice!;
+		const { channel: botVoiceChannel } = message.guild.me?.voice!;
+		const formatValidator = /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/g;
+
 		try {
+			const seekAmmount = await args.rest('string');
+
+			const timeArray = seekAmmount.split(':').map((value) => {
+				return parseInt(value, 10);
+			});
+
+			if (!userVoiceChannel) {
+				embedReply.setDescription('You have to be connected to a voice channel before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (userVoiceChannel.id !== botVoiceChannel?.id) {
+				embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer) {
+				embedReply.setDescription("There isn't an active player on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer.playing && !erelaPLayer.paused) {
+				embedReply.setDescription("There's nothing currently playing on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!formatValidator.test(seekAmmount)) {
+				embedReply.setDescription('Invalid time format!, usage: `%seek hh:mm:ss`, `%seek mm:ss`, `%seek ss`');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			let seekSeconds;
+
+			switch (timeArray.length) {
+				case 3: {
+					seekSeconds = timeArray[0] * 3600 + timeArray[1] * 60 + +timeArray[2];
+					break;
+				}
+				case 2: {
+					seekSeconds = timeArray[0] * 60 + +timeArray[1];
+					break;
+				}
+				case 1: {
+					seekSeconds = timeArray[0];
+					break;
+				}
+				default: {
+					embedReply.setDescription('Invalid time format!, usage: `%seek hh:mm:ss`, `%seek mm:ss`, `%seek ss`');
+					return message.reply({ embeds: [embedReply] });
+				}
+			}
+
+			if (seekSeconds > erelaPLayer.queue.current?.duration! / 1000 - 1) {
+				embedReply.setDescription("The specified time is longer that the current track's length!").setColor('RED');
+				return message.channel.send({ embeds: [embedReply] });
+			}
+
+			erelaPLayer.seek(seekSeconds * 1000);
+
 			return message.reply(':(');
 		} catch (error: any) {
+			if (error.identifier === 'argsMissing') {
+				embedReply.setDescription('You must specify the ammount of time to seek!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
 			this.container.client.logger.error(`There was an unexpected error in command "${this.name}"`, error);
 			embedReply.setDescription('There was an unexpected error while processing the command, try again later.');
 			return message.reply({ embeds: [embedReply] });
