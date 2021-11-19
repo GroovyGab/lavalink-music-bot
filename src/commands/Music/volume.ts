de4904052e8bf6cf45faed8eb152e3e26c5bd00b
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-19 12:42:47 -0300

    Testing something

--- src/commands/Music/volume.ts
+++ src/commands/Music/volume.ts
@@ -2,7 +2,7 @@
  * Module imports.
  */
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command, CommandOptions } from '@sapphire/framework';
+import { Args, Command, CommandOptions } from '@sapphire/framework';
 import { Message, MessageEmbed } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
@@ -11,23 +11,36 @@ import { Message, MessageEmbed } from 'discord.js';
 	fullCategory: ['music']
 })
 export class UserCommand extends Command {
-	public async messageRun(message: Message) {
+	public async messageRun(message: Message, args: Args) {
+		if (!message.guild) return;
+		const erelaPLayer = this.container.client.players.get(message.guild.id);
 		const embedReply = new MessageEmbed();
+		const { channel: userVoiceChannel } = message.member?.voice!;
+		const { channel: botVoiceChannel } = message.guild.me?.voice!;
+
 		try {
-			/*if (!message.guild) return;
-			const data = {
-				op: 'filters',
-				guildId: message.guild.id,
-				karaoke: {
-					level: 1.0,
-					monoLevel: 1.0,
-					filterBand: 220.0,
-					filterWidth: 100.0
-				}
-			};
-			const a = await this.container.client.players.get(message.guild.id)?.node.send(data);
-			return console.log(a);*/
-			return;
+			const volume = args.rest('float');
+			if (!userVoiceChannel) {
+				embedReply.setDescription('You have to be connected to a voice channel before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer) {
+				embedReply.setDescription("There isn't an active player on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer.playing && !erelaPLayer.paused) {
+				embedReply.setDescription("There's nothing currently playing on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (userVoiceChannel.id !== botVoiceChannel?.id) {
+				embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			return console.log(volume);
 		} catch (error: any) {
 			this.container.client.logger.error(`There was an unexpected error in command "${this.name}"`, error);
 			embedReply.setDescription('There was an unexpected error while processing the command, try again later.');
