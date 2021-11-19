2a1a44a30215d1d4bc0315916e301f0eb40c7383
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-19 16:16:49 -0300

    feat(nowplaying): added nowplaying command

--- src/commands/Music/nowplaying.ts
+++ src/commands/Music/nowplaying.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { Message, MessageEmbed } from 'discord.js';
+import { splitBar } from 'string-progressbar';
 
 @ApplyOptions<CommandOptions>({
 	name: 'nowplaying',
@@ -10,13 +11,62 @@ import { Message, MessageEmbed } from 'discord.js';
 })
 export class UserCommand extends Command {
 	public async messageRun(message: Message) {
+		if (!message.guild) return;
+		const erelaPLayer = this.container.client.players.get(message.guild.id);
 		const embedReply = new MessageEmbed();
+		const { channel: userVoiceChannel } = message.member?.voice!;
+		const { channel: botVoiceChannel } = message.guild.me?.voice!;
 		try {
-			return message.reply(':(');
+			if (!userVoiceChannel) {
+				embedReply.setDescription('You have to be connected to a voice channel before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (userVoiceChannel.id !== botVoiceChannel?.id) {
+				embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer) {
+				embedReply.setDescription("There isn't an active player on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPLayer.playing && !erelaPLayer.paused) {
+				embedReply.setDescription("There's nothing currently playing on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			const trackIsStream = erelaPLayer.queue.current?.isStream;
+			const trackPosition = erelaPLayer.position;
+			const trackLength = erelaPLayer.queue.current?.duration!;
+
+			const trackInfo = erelaPLayer.queue.current;
+			const splitProgressBar = trackIsStream ? '' : splitBar(trackLength, trackPosition, 20, '▬', '🔵')[0];
+
+			const trackProgress = trackIsStream ? '[◉ LIVE]' : `${this.msToHMS(trackPosition)}`;
+			const totalTrackLength = trackIsStream ? '' : `${this.msToHMS(trackLength)}`;
+			const embedFooter = trackIsStream ? trackProgress : `${splitProgressBar} ${trackProgress} / ${totalTrackLength}`;
+
+			embedReply.setDescription(`[${trackInfo?.title}](${trackInfo?.uri}) [${trackInfo?.requester}]`).setFooter(embedFooter);
+			return message.reply({ embeds: [embedReply] });
 		} catch (error: any) {
 			this.container.client.logger.error(`There was an unexpected error in command "${this.name}"`, error);
 			embedReply.setDescription('There was an unexpected error while processing the command, try again later.');
 			return message.reply({ embeds: [embedReply] });
 		}
+	}
+
+	private msToHMS(ms: number) {
+		let seconds = ms / 1000;
+
+		const hours = Math.floor(seconds / 3600);
+		seconds = seconds % 3600;
+
+		const minutes = Math.floor(seconds / 60);
+
+		seconds = Math.floor(seconds % 60);
+
+		return `${hours}h ${minutes}m ${seconds}s`;
 	}
 }
