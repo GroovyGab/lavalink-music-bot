cc0dc60d958216009bc67c35b3e688bbe9fc0746
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-19 02:52:50 -0300

    e

--- src/listeners/voiceStateUpdate.ts
+++ src/listeners/voiceStateUpdate.ts
@@ -12,7 +12,6 @@ export class UserEvent extends Listener<typeof Events.VoiceStateUpdate> {
 
 	public run(_oldState: VoiceState, _newState: VoiceState) {
 		// This is a bad system
-		// :"v
 		/*const erelaPLayer = this.container.client.players.get(oldState.guild.id);
 
 		if (erelaPLayer) {
