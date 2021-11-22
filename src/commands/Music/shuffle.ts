315e06f0980c01619244bfc84605fcb7050fff85
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-22 15:14:20 -0300

    Add shuffle command.

--- src/commands/Music/shuffle.ts
+++ src/commands/Music/shuffle.ts
@@ -7,6 +7,7 @@ import { Message, MessageEmbed } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
 	name: 'shuffle',
+	aliases: ['random'],
 	description: 'Shuffles the queue',
 	fullCategory: ['music']
 })
@@ -16,9 +17,34 @@ export class ShuffleCommand extends Command {
 		if (!message.member) return;
 		if (!message.guild.me) return;
 
+		const erelaPlayer = this.container.client.manager.get(message.guild.id);
 		const embedReply = new MessageEmbed();
+		const userVoiceChannel = message.member.voice.channel;
+		const botVoiceChannel = message.guild.me.voice.channel;
+
 		try {
-			return message.reply(':(');
+			if (!userVoiceChannel) {
+				embedReply.setDescription('You have to be connected to a voice channel before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (erelaPlayer && botVoiceChannel && userVoiceChannel.id !== botVoiceChannel.id) {
+				embedReply.setDescription('You need to be in the same voice channel as the bot before you can use this command!');
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if (!erelaPlayer) {
+				embedReply.setDescription("There isn't an active player on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			if ((!erelaPlayer.playing && !erelaPlayer.paused) || !erelaPlayer.queue.current) {
+				embedReply.setDescription("There's nothing currently playing on this server!");
+				return message.reply({ embeds: [embedReply] });
+			}
+
+			erelaPlayer.queue.shuffle();
+			return message.react('🔀');
 		} catch (error: any) {
 			this.container.client.logger.error(`There was an unexpected error in command "${this.name}"`, error);
 			embedReply.setDescription('There was an unexpected error while processing the command, try again later.');
