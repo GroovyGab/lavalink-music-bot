c62431fa0192e7783db8c3b48e1c7a347698df18
GaboTheCoder <ghernandezlohaus.78@gmail.com>
2021-11-23 14:20:45 -0300

    remove unecesarry stuff

--- src/Argument.d.ts
+++ src/Argument.d.ts
@@ -2,12 +2,11 @@ import type { Collection } from 'discord.js';
 import type { Player } from 'erela.js';
 import type { LavalinkHandler } from './structures/Music/LavalinkHandler';
 import type { Queue } from './structures/Music/Queue';
-import type { SlashCommandStore } from './structures/SlashCommands/SlashCommandStore';
+import type { SlashCommandStore } from '../extra/SlashCommands/SlashCommandStore';
 
 declare module 'discord.js' {
 	interface Client {
 		manager: LavalinkHandler;
-		timeouts: Collection<string, NodeJS.Timeout>;
 		sleep(ms: number): Promise<unknown>;
 	}
 }
--- src/slashCommandBuilders/ping.ts
+++
@@ -1,5 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-const slashCommand = new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!').toJSON();
-
-export { slashCommand };
