1944d9d681c00ea9c062db5bdee2583a93550826
Gabriel Hernández <ghernandezlohaus.78@gmail.com>
2022-03-26 23:11:45 -0300

    HOLA EÖA BANDA

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { MusicBotClient } from './structures/Client';
 
+
 const client = new MusicBotClient();
 
 (async () => {
--- src/listeners/music/queueEnd.ts
+++ src/listeners/music/queueEnd.ts
@@ -13,6 +13,36 @@ export class NodeReconnectEvent extends Listener {
 
 	public run(player: Player) {
 		if (!player.textChannel) return;
+		const erelaPlayer = this.container.client.manager.get(player.guild);
+
+		if (erelaPlayer) {
+			const voiceChannel = this.container.client.channels.cache.get(player.voiceChannel!);
+			if (voiceChannel?.isVoice()) {
+				this.container.logger.debug(`Starting inactivity timeout in: ${voiceChannel.name} (${voiceChannel.id})`);
+				if (!erelaPlayer.existingLeaveTimeout) {
+					erelaPlayer.existingLeaveTimeout = true;
+					setTimeout(() => {
+						if (!player.playing) {
+							const channel = this.container.client.channels.cache.get(erelaPlayer.textChannel!);
+
+							if (channel && channel.isText()) {
+								// Groovy message lol
+
+								// I left the voice channel because I was inactive for too long. If you are a Premium member, you can disable this by typing /247.
+
+								const embed = new MessageEmbed().setDescription(`I left the voice channel because I was inactive for too long`);
+								channel.send({ embeds: [embed] });
+							}
+							erelaPlayer.destroy();
+						} else {
+							this.container.logger.info(
+								`The bot will not disconnect, the player is active in ${voiceChannel.name} [${voiceChannel.id}] from guild ${voiceChannel.guild.name} [${voiceChannel.id}]`
+							);
+						}
+					}, parseInt(process.env.DISCONNECT_DELAY!) * 1000);
+				}
+			}
+		}
 		const embedReply = new MessageEmbed();
 
 		embedReply.setDescription(`Queue has ended, feel free to add more songs using  \`${process.env.PREFIX}play\`.`);
